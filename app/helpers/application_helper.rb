807ec54355954fb7eb150b5743aa7ddcf96499ce
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2017-05-21 16:40:26 +0000

    TOPページのレイアウトを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def page_title
+        title = 'いとしま子ども起業家塾'
+        title = @page_title + "-"+ title if @page_title
+        title
+end
+
+    def menu_link_to(text, path)
+            link_to_unless_current(text, path){content_tag(:span, text) }
+        end
 end
