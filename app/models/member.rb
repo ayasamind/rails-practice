898dad55dbe8a47e23f4b41a2d1aebe959700346
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2017-05-26 15:58:28 +0000

    フォームのバリデーションを追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,26 @@
 class Member < ActiveRecord::Base
-    class << self
+    include EmailAddressChecker
+        validates :number, presence: true,
+            numericality: { only_integer: true, greater_than: 0, less_than: 100, allow_brank: true },
+            uniqueness: true
+        
+        validates :name, presence: true,
+            format: { with: /\A[A-Za-z]\w*\z/, allow_brank: true }, #空を禁止、英数字のみ、文字列の先頭はアルファベット
+                    length: {minumum: 2, maximum: 20, allow_brank: true },#2文字以上20文字以下
+                    uniqueness: {case_sensitive: false} #重複を禁止
+        
+        validates :full_name, length: {maximum: 20}
+        
+        validate :check_email
+        
+        private
+        def check_email
+            if email.present?
+                errors.add(:email, :invalid) unless well_formed_as_email_address(email)
+            end
+        end
+    
+    class << self    
         def search(query)
             rel = order("number")
             if query.present?
