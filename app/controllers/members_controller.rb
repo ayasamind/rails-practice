894284d7c1ea672cffd741938e39ddd2c3e98598
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2017-05-26 14:30:37 +0000

    新規登録、編集画面のviewを作成

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -10,9 +10,11 @@ class MembersController < ApplicationController
     end
     
     def new
+        @member = Member.new(birthday:Date.new(1980, 1, 1))
     end
     
     def edit
+        @member = Member.find(params[:id])
     end
     
     def create
